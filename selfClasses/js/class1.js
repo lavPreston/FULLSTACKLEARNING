c267317cf0578a8b251b460fd70b30deb0d52f0e
lavPreston <lavanyarajendran964@gmail.com>
2026-02-22 22:44:08 +0530

    adding js folder

---
+++ selfClasses/js/class1.js
@@ -0,0 +1,19 @@
+//exercise 1
+let length =10;
+let breadth=20;
+let area=length*breadth;
+console.log("Area of the rectange is " + area)
+
+//exercise 2
+let principal =100;
+let rate=10;
+let time=2;
+let si=((principal*rate*time)/100);
+console.log("the simple interest is "+si);
+
+//exercise 3
+let item1Price =10;
+let item2Price=20;
+let item3Price=50;
+let total=item1Price+item2Price+item3Price;
+console.log("the total bill amount is "+total);
